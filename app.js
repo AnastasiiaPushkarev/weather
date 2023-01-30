e28f7649af828a9167304997252bab58c1bc6bdf
Anastasiia Pushkareva <anastasiapush09@gmail.com>
2023-01-30 15:59:28 -0500

    Get temperature, weather condition and icon from API

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const https = require("https");
 const app = express();//initialize new express app
 
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Kingston&appid=c6a6f51867cd07a6784e02c37cc6394d";
+    const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Kingston, ON, CA&appid=c6a6f51867cd07a6784e02c37cc6394d";
     
     https.get(url, function(response){
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in Kingston, ON is " + temp + " degrees Celcius.</h1>");
+            res.write("<img src=" + imageURL +">");
+            res.send();
+        })
     })
 
-    res.send("Server is up and running.")
+    // res.send("Server is up and running.")
 })
 
 
